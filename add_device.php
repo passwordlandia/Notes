e1c2ff5443705fceab3342e44ec584d017f39d5b
passwordlandia <38091593+passwordlandia@users.noreply.github.com>
2018-06-12 04:48:30 -0700

    cacti spin up for ec

---
+++ add_device.php
@@ -0,0 +1,39 @@
+usage: add_device.php --description=[cactifinalserver] --ip=[10.142.0.14] --template=[focused-outlook-191902]
+    --ping_method=[icmp] --ping_port=[N/A, 1-65534] --ping_retries=[2]
+    [--port=161] [--timeout=500]
+    [--username=cacti --password=cactipass] 
+Required:
+    --description  cactifinalserver
+    --ip           10.142.0.14
+
+usage: add_device.php --description=[djangofinalserver] --ip=[10.142.0.27] --template=[focused-outlook-191902]
+    --ping_method=[icmp] --ping_port=[N/A, 1-65534] --ping_retries=[2]
+    [--port=161] [--timeout=500]
+    [--username=cacti --password=cactipass] 
+Required:
+    --description  cactifinalserver
+    --ip           10.142.0.27
+    
+    usage: add_device.php --description=[rsyslogfinalserver] --ip=[10.142.0.15] --template=[focused-outlook-191902]
+    --ping_method=[icmp] --ping_port=[N/A, 1-65534] --ping_retries=[2]
+    [--port=161] [--timeout=500]
+    [--username=cacti --password=cactipass] 
+Required:
+    --description  rsyslogfinalserver
+    --ip           10.142.0.15
+    
+    usage: add_device.php --description=[postgresfinalserver] --ip=[10.142.0.12] --template=[focused-outlook-191902]
+    --ping_method=[icmp] --ping_port=[N/A, 1-65534] --ping_retries=[2]
+    [--port=161] [--timeout=500]
+    [--username=cacti --password=cactipass] 
+Required:
+    --description  postgresfinalserver
+    --ip           10.142.0.12
+    
+    usage: add_device.php --description=[cactifinalserver] --ip=[10.142.0.14] --template=[focused-outlook-191902]
+    --ping_method=[icmp] --ping_port=[N/A, 1-65534] --ping_retries=[2]
+    [--port=161] [--timeout=500]
+    [--username=cacti --password=cactipass] 
+Required:
+    --description  cactifinalserver
+    --ip           10.142.0.14
